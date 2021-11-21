b8f506336518549feb561973fd0eb380eedf142e
tushar517 <tusharrawat517@gmail.com>
2021-11-21 14:59:07 +0000

    made response for post method

--- src/main/java/com/example/RestAPI/student/Response.java
+++ src/main/java/com/example/RestAPI/student/Response.java
@@ -9,6 +9,22 @@ public class Response {
         this.statusMessage = "Student Created Successfully";
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
     @Override
     public String toString() {
         return "Response{" +
